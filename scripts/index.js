fe02edf3c734175cd4f269d253fcbbb091d054d4
Marian <dudychmarian@gmail.com>
2020-07-27 14:37:39 +0300

    Upload project
    Upload:
    1)HTML
    2)CSS
    3)JS

---
+++ scripts/index.js
@@ -0,0 +1,23 @@
+import { radioPlayerInit } from './radioPlayer.js';
+import { videoPlayerInit } from './videoPlayer.js';
+import { musicPalyerInit } from './musicPlayer.js';
+
+const playerBtn = document.querySelectorAll('.player-btn');
+const playerBlock = document.querySelectorAll('.player-block');
+const temp = document.querySelector('.temp')
+
+
+const deactivationPlayer = () => {
+    temp.style.display= 'none';
+    playerBtn.forEach(item => item.classList.remove('active'));
+    playerBlock.forEach(item => item.classList.remove('active'));
+};
+
+
+playerBtn.forEach((btn, i) => btn.addEventListener('click', () => {
+    deactivationPlayer();
+    btn.classList.add('active');
+    playerBlock[i].classList.add('active');
+    }));
+
+radioPlayerInit();
